7fc30b01babbd005560772fccfbb0adbbd0c84b9
SyedZaid <syedmzaid.99@gmail.com>
2024-01-01 09:50:03 +0500

    FCFS DOne

--- FCFS_new.py
+++ FCFS_new.py
@@ -10,36 +10,49 @@ def FCFS(inp_arrival_times, inp_burst_times):
     print("Sorted Process List:", inp_process_list.__getlist__(), '\n\n')
 
     curBT, prevBT = 0, 0
-    curProcess = inp_process_list.pop()
-    curAT, prevAT, nextAT = curProcess.arrival_time, 0, 0
-
+    curProcess = 0
+    processAT= 0
+    time_pointer = 0
     idleTime, total_idle_time = 0, 0
 
-    completion_time = 0
-    execution_sequence = []
     gantt_chart = []  # Process_name,Start_Time, Completion_Time
     # print("Total-BurstTime:",inp_process_list.__getTotalBurstTime__())
-
-    for process_num in range(inp_process_list.length() + total_idle_time):
+    print(inp_process_list.__getlist__())
+    for process_num in range(0,inp_process_list.length()):
+        curProcess = inp_process_list.__getprocess__(process_num)
+        processAT = curProcess.arrival_time
+        
 
         print("Current Process", curProcess.process_name)
-        print("curAT, curBT, prevBT", curAT, curBT, prevBT)
+        print("ProcessAT, curBT, prevBT", processAT, curBT, prevBT,time_pointer)
+        print("Timepointer:",time_pointer)
 
-        if curAT > curBT:
-            idleTime = curAT - curBT
-            gantt_chart.append([curBT, 'Idle', curAT])
+        if processAT > time_pointer:
+            idleTime = processAT - time_pointer
+            
+            time_pointer += total_idle_time
             total_idle_time += idleTime
+            gantt_chart.append([processAT-idleTime, 'Idle',idleTime, time_pointer,total_idle_time])
+        
+        if processAT < time_pointer:
+            processAT = time_pointer
 
-        gantt_chart.append([curAT, curProcess.process_name, curAT + curProcess.burst_time])
-
-        curProcess = inp_process_list.pop()
-        curAT = curProcess.arrival_time
-        curBT = curProcess.burst_time
+        time_pointer = processAT + curProcess.burst_time # 3,
+        
+        gantt_chart.append([processAT, curProcess.process_name,time_pointer])
+        curBT = curProcess.burst_time + prevBT
+        prevBT += curBT
+        
 
     print("Gantt Chart:", gantt_chart)
     print("Execution Sequence:", [entry[1] for entry in gantt_chart])
 
 # Input examples from the Front-end.
-arrival_times = [1, 4, 5, 6]
-burst_times = [2, 3, 2, 1]
+# arrival_times = [1, 4, 5, 6]
+# burst_times = [2, 3, 2, 1]
+# input1 = FCFS(arrival_times, burst_times)
+
+
+arrival_times = [1,10,5,6]
+burst_times = [2,3,3,10]
 input1 = FCFS(arrival_times, burst_times)
